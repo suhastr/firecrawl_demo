4d99379b95c183bbe2a85cfee63a13b9e6deecc9
suhas <36911828+suhastr@users.noreply.github.com>
2025-06-29 17:50:55 -0400

    added some files and deleted some

---
+++ llm_extraction_demo.py
@@ -0,0 +1,29 @@
+from firecrawl import JsonConfig, FirecrawlApp
+from pydantic import BaseModel
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+api_key = os.getenv("FIRECRAWL_API_KEY")
+
+app = FirecrawlApp(api_key=api_key)
+
+class ExtractSchema(BaseModel):
+    company_mission: str
+    supports_sso: bool
+    is_open_source: bool
+    is_in_yc: bool
+
+json_config = JsonConfig(
+    schema=ExtractSchema
+)
+
+llm_extraction_result = app.scrape_url(
+    'https://suhastr.github.io/',
+    formats=["json"],
+    json_options=json_config,
+    only_main_content=False,
+    timeout=120000
+)
+
+print(llm_extraction_result.json)
---
+++ map_demo.py
@@ -0,0 +1,24 @@
+# Input a website and get all the urls on the website - extremely fast
+
+
+from firecrawl import FirecrawlApp
+import os
+from dotenv import load_dotenv
+
+# Load API key from .env
+load_dotenv()
+api_key = os.getenv("FIRECRAWL_API_KEY")
+
+
+import asyncio
+from firecrawl import AsyncFirecrawlApp
+
+async def main():
+    app = AsyncFirecrawlApp(api_key=api_key)
+    response = await app.map_url(
+        url='https://suhastr.github.io/',
+        include_subdomains=True
+    )
+    print(response)
+
+asyncio.run(main())
