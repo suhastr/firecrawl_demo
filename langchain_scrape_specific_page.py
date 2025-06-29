af928f55a923cb5471034e3bec02ab4114d2851b
suhas t r <trsuhas21@gmail.com>
2025-06-29 17:57:31 -0400

    Update langchain_scrape_specific_page.py

--- langchain_scrape_specific_page.py
+++ langchain_scrape_specific_page.py
@@ -8,7 +8,7 @@ api_key = os.getenv("FIRECRAWL_API_KEY")
 
 # Correct way: use 'params' to pass options like formats, etc.
 loader = FireCrawlLoader(
-    url="https://suhastr.github.io/",
+    url="website url",
     api_key=api_key,
     mode="scrape",  # or "crawl", "extract", etc.
     params={
